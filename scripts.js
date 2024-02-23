5d79032627a82fe6b9502cc236d0352191a24cdf
Nidup Dorji <needup444@gmail.com>
2024-02-23 21:03:51 +0530

    dark light theme

--- scripts.js
+++ scripts.js
@@ -6,3 +6,22 @@ function toggleMenu() {
 }
 
 document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+
+//toggle theme
+const darkModeToggle = document.getElementById('toggleTheme');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
+
+// Check if dark mode is enabled from previous session
+if (localStorage.getItem('darkMode') === 'enabled') {
+    body.classList.add('dark-mode');
+}
